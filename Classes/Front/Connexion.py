adcb4b0cad66f80486f9604819e686f65bf5b15b
maysa-bik <maysa.bik@laplateforme.com>
2024-04-01 00:44:01 +0200

    rajouter tout les dessine

--- Classes/Front/Connexion.py
+++ Classes/Front/Connexion.py
@@ -1,7 +1,7 @@
 import pygame 
 from pygame.locals import *
 
-from Button import Button 
+from Button import Button, EyeButton
 pygame.init()
 
 BK = pygame.display.set_mode((1000, 700))
@@ -49,10 +49,22 @@ while True:
         if event.type == pygame.QUIT:
             quit()
         elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:  # Clic gauche
+                if eye_button.x < event.pos[0] < eye_button.x + eye_button.width and \
+                        eye_button.y < event.pos[1] < eye_button.y + eye_button.height:
+                    # Si le clic est sur le bouton de l'œil
+                    eye_button.toggle_password_visibility()    
+        """
+        elif event.type == MOUSEBUTTONDOWN:
             if connexion_button.is_clicked(mouse_pos):
                 pass
             # Gérer les clics de souris pour les interactions avec les champs d'entrée
-    
+            elif event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:  # Clic gauche
+                    if 800 < event.pos[0] < 850 and 255 < event.pos[1] < 305:
+                        # Si le clic est sur le bouton de l'œil
+                        eye_button.toggle_password_visibility()
+        """
     
     # Création d'une surface de texte pour le titre "Budget buddy"
     t_font = pygame.font.Font("Classes/Images/CFAzteques-Regular.ttf", 50)  # Choisir la police et la taille
@@ -72,28 +84,43 @@ while True:
 
     # Créer un rectangle pour le bouton
     google_rect = image_logo.get_rect()
-    google_rect.topleft = (250, 400)  # Position sous le texte "Bienvenue"
+    google_rect.topleft = (220, 400)  # Position sous le texte "Bienvenue"
 
-    
+    # Charger les images des yeux
+    open_eye_image = pygame.image.load('Classes/Images/eye-24.png')
+    closed_eye_image = pygame.image.load('Classes/Images/closed-eye-24.png')
+
+    # Créer l'objet EyeButton
+    eye_button = EyeButton(800, 320, 50, 50, open_eye_image, closed_eye_image, BK)
 
     BK.blit(image, (0, 0))       
     pygame.draw.rect(BK, L, (x, y, width, height))
     # Dessiner les champs d'entrée et les étiquettes
     pygame.draw.rect(BK, L, (x, y, 250, 40))  # Rectangle pour le nom d'utilisateur
-    text_surface = font.render("Nom ou Email", True, (0, 0, 0))  # Étiquette pour le nom d'utilisateur
+    text_surface = font.render("Nom ou Email", True, N)  # Étiquette pour le nom d'utilisateur
     BK.blit(text_surface, (570, 205))
     pygame.draw.rect(BK, N, (570, y + 180, 250, 3))  # Dessiner une ligne sous le texte "Nom ou Email"
     pygame.draw.rect(BK, L, (x, y, 250, 40))  # Rectangle pour le mot de passe
-    text_surface = font.render("Mot de passe", True, (0, 0, 0))  # Étiquette pour le mot de passe
+    text_surface = font.render("Mot de passe", True, N)  # Étiquette pour le mot de passe
     BK.blit(text_surface, (570, 300))
     pygame.draw.rect(BK, N, (570, y + 270, 250, 3))  # Dessiner une ligne sous le texte "Mot de passe"
     connexion_button = Button(570, 400, 250, 40, N, "Se connecter", font)
-    
     connexion_button.draw_button(BK)
+
+
     # Affichage du titre à l'écran
     BK.blit(t_text, t_rect)
     # Affichage du mot "Bienvenue" à l'écran
     BK.blit(B_text, B_rect)
     # Afficher l'image du bouton
     BK.blit(image_logo, google_rect)
+    # Dessiner le bouton de l'œil
+    eye_button.draw()
+
+    # Création du bouton "Mot de passe oublié ?"
+    MDP_O_font = pygame.font.Font(None, 20)
+    MDP_O_text = MDP_O_font.render("Mot de passe oublié ?", True, N)
+    MDP_O_rect = MDP_O_text.get_rect(topleft=(680, 350))
+    BK.blit(MDP_O_text, MDP_O_rect)
+
     pygame.display.update()
--- Classes/Front/button.py
+++ Classes/Front/button.py
@@ -17,3 +17,24 @@ class Button:
 
     def is_clicked(self, mouse_pos):
         return self.rect.collidepoint(mouse_pos)
+    
+class EyeButton:
+    def __init__(self, x, y, width, height, open_eye_image, closed_eye_image, screen):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.open_eye_image = open_eye_image
+        self.closed_eye_image = closed_eye_image
+        self.screen = screen
+        self.show_password = False
+
+    def draw(self):
+        if self.show_password:
+            self.screen.blit(self.open_eye_image, (self.x, self.y))
+        else:
+            self.screen.blit(self.closed_eye_image, (self.x, self.y))
+
+    def toggle_password_visibility(self):
+        self.show_password = not self.show_password
+
